ee8949a293027e07c0ca703181887c77cc869977
Manan <manantheassassin@gmail.com>
2023-07-30 15:04:56 +0530

    Added Computer 3D modal in hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, SpotLight, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={2} groundColor="black" />
+      <pointLight intensity={3} />
+      {/* <SpotLight
+        position={[0, 1, 0]}
+        penumbra={1}
+        distance={7}
+        angle={0.12}
+        attenuation={5}
+        anglePower={3}
+        radiusBottom={5}
+        radiusTop={1}
+        intensity={1}
+        castShadow
+        shadowMapWidth={1024}
+        shadowMapHeight={1024}
+      /> */}
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputerCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          // minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputerCanvas;
